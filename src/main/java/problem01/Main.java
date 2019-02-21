1034e6a0c919b0c8fd33413783d3a078ca797025
BIT <BIT@175.209.90.185>
2019-02-21 10:51:57 +0900

    solved

--- src/main/java/problem01/Main.java
+++ src/main/java/problem01/Main.java
@@ -1,16 +1,58 @@
 package problem01;
 
+import java.util.Arrays;
+
 public class Main {
 	public static void main(String[] args) {
 		int[] arr = { -10, -3, 5, 6, -20 };
+	
 		findMaxProduct(arr);
 	}
 
 	public static void findMaxProduct(int[] arr) {
 		
-		//
-		// 코드를 완성 하십시오.
-		//
+		int min1 = 0, min2 = 0, max1 = 0 , max2 = 0, max=0;
+		int check=0;
+		for(int i=0;i<arr.length;i++) {
+			if(arr[i]<0) {
+				
+				if(min1 > arr[i] || min2 > arr[i]) {
+					if(min1>=min2) {
+						min1=arr[i];
+					}
+					else
+						min2=arr[i];
+					if(max < min1*min2) {
+						max = min1*min2;
+						check=0;
+					}
+					continue;
+				}
+				
+			}
+			if(arr[i]>0) {
+				if(max1 < arr[i] || max2 < arr[i]) {
+					if(max1<=max2) {
+						max1=arr[i];
+					}
+					else
+						max2=arr[i];
+					if(max < max1*max2) {
+						max = max1*max2;
+						check=1;
+					}
+						
+					continue;
+				}
+				
+			}
+			
+		}
 		
+		if(check == 0) {
+			System.out.println("[" + min1 + "," + min2 + "]");
+		}
+		else
+			System.out.println("[" + max1 + "," + max2 + "]");
 	}
 }
--- src/main/java/problem03/Money.java
+++ src/main/java/problem03/Money.java
@@ -6,25 +6,59 @@ public class Money {
 	// 클래스 구현을 완성 하십시오.
 	// 
 
-	
 	private int amount;
 	
+	
 	public Money(int amount) {
+		this.amount = amount;
 	}
+	
 
 	public Money add(Money money) {
-		return null;
+		Money temp = new Money(0);
+		temp.amount = money.amount + this.amount; 
+		return temp;
 	}
 
 	public Money minus(Money money) {
-		return null;
+		Money temp = new Money(0);
+		temp.amount = this.amount - money.amount; 
+		return temp;
 	}
 
 	public Money multiply(Money money) {
-		return null;
+		Money temp = new Money(0);
+		temp.amount = this.amount * money.amount; 
+		return temp;
 	}
 
 	public Money devide(Money money) {
-		return null;
+		Money temp = new Money(0);
+		temp.amount = this.amount / money.amount; 
+		return temp;
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+
+	    if (obj == null) {
+	        return false;
+	    }
+
+	    if (this.getClass() != obj.getClass()) {
+	        return false;
+	    }
+
+	    if (this == obj) {
+	        return true;
+	    }
+
+	    Money that = (Money) obj;
+  
+	    if (this.amount == that.amount ) {
+	        return true;
+	    }
+
+	    return false;
 	}
 }
