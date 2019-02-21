fa37c15db227eaeea5bf7eb097384127cf5ffa97
BIT <BIT@175.209.90.185>
2019-02-21 11:00:21 +0900

    solved

--- src/main/java/problem04/Main.java
+++ src/main/java/problem04/Main.java
@@ -1,9 +1,30 @@
 package problem04;
 
+import java.util.Random;
+
 public class Main {
 	
 	public static void main(String[] args) {
-
+		Random ran = new Random();
+		int snum=0, onum=0;
+		
+		for(int i=0;i<20;i++) {
+			if(ran.nextInt(2)==1) {
+				if(snum==10)
+					continue;
+				snum++;
+				System.out.println("#");
+			}
+			else {
+				if(onum==10)
+					continue;
+				onum++;
+				System.out.println("O");
+			}
+		}
+		
+		
+			
 	}
 
 }
